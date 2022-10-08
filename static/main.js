180238b4e7d817841269b2524f6faa86e6ea4caa
avdivo <avdivo@mail.ru>
2022-10-08 15:04:12 +0300

    JS. Переключение фильтров

--- static/main.js
+++ static/main.js
@@ -7,20 +7,23 @@ class Lists {
     constructor(element) {
         this.element = element // Объекд в DOM отвечающий за выбор фильтра
         this.list = [];  // Список выбранных букв
-        this.old_class = element.attr('class');
+        this.old_bg = element.css('background-color'); // Запомнить цвет фона
+        this.old_ink = element.css('color'); // Запомнить цвет текста
         Lists.obj.push(this)
     }
 
     // Активация фильтра (изменение вида переключателя)
     activate() {
         Lists.deactivate();
-        this.element.attr('class', 'btn btn-primary');
+        this.element.css({'background-color': '#0d6efd'});
+        this.element.css({'color': 'white'});
     }
 
     // Деактивация всех переключателей (возврат в исходное состояние)
     static deactivate() {
         Lists.obj.forEach(function(item, i, arr) {
-            item.element.attr('class', item.old_class);
+            item.element.css({'background-color': item.old_bg});
+            item.element.css({'color': item.old_ink});
         });
     }
 }
@@ -28,9 +31,13 @@ class Lists {
 // Класс для букв наследуем от общего, добавляем свойства и методы
 class Letters extends Lists {
     constructor(element) {
-        this.element = element
+        super(element)
+        this.element = element // Объекд в DOM отвечающий за выбор фильтра
         this.list = [];  // Список выбранных букв
+        this.old_bg = element.css('background-color');
+        this.old_ink = element.css('color');
         this.here = false;  // Определяет, стоит ли в этой позиции буква из списка. Или она в этом слове, но не здксь
+        Lists.obj.push(this)
     }
 
 }
@@ -43,12 +50,16 @@ $(document).ready(function(){
     var white_list = new Lists($('#white_list'));  // Белый список. Объект
     var double_letter = new Lists($('#double_letter'));  // Дубликаты букв
     var existing_letters = [];  // Массив с объектами для каждоой буквы
+    for (let i = 1; i < 6; i++) {
+    alert($('#l' + i).attr('id'))
+        existing_letters.push(new Letters($('#l' + i)))
+    }
 //    var kb = Keyboard();  // Инициализация клавиатуры
 
 //    var ban = false;  // Запрет дубликатов букв
 
 
-//    alert(double_letter.element.css({'background-color' : '#ff0000'}));
+//double_letter.element.css({'background-color' : '#ff0000'});
 
 
 
